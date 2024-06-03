6b8663e433d5c3f195e85786133d97a6b19b8a30
Adam Naghavi <adamnaghavif@gmail.com>
2024-06-03 06:51:56 -0400

    Slowly making error version of vec

---
+++ vector copy.c
@@ -0,0 +1,343 @@
+#include "vector.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef DISABLE_ASSERT
+#define ASSERT(cond)                                                                                \
+    if (!(cond))                                                                                    \
+    {                                                                                               \
+        printf("Assertion Failed:\n\tLine: %d\n\tFile: %s\n\t\"%s\"\n", __LINE__, __FILE__, #cond); \
+        exit(1);                                                                                    \
+    }
+#else
+#define ASSERT(cond) cond
+#endif
+
+#define PANIC(str)           \
+    do                       \
+    {                        \
+        printf("%s\n", str); \
+        exit(1);             \
+    } while (0)
+
+#define EXIT_IF_NULL(value)                                                                   \
+    if (value == NULL)                                                                        \
+    {                                                                                         \
+        fprintf(stderr, "Given null vector on line %d in file \"%s\"\n", __LINE__, __FILE__); \
+        exit(1);                                                                              \
+    }
+
+#define RET_IF_NULL(value, return_value)                                             \
+    if (value == NULL)                                                               \
+    {                                                                                \
+        fprintf(stderr, "Given null on line %d in file \"%s\"", __LINE__, __FILE__); \
+        return return_value;                                                         \
+    }
+
+#define VALIDATE_VECTOR(vec) EXIT_IF_NULL(vec)
+
+static size_t default_growth_rate(Vec *v)
+{
+    return v->capacity * 2;
+}
+
+static int default_cmp(const void *data0, const void *data1)
+{
+    /* compare the first byte */
+    if (!(data0 && data1))
+        return 0;
+    char d0 = *(char *)data0, d1 = *(char *)data1;
+    if (d0 == d1)
+        return 0;
+    if (d0 > d1)
+        return 1;
+    return -1;
+}
+
+Vec *vec_new0(size_t capacity, size_t elem_size, void_cmp_func cmp, vec_growth_rate_func grow, void (*free_entry)(const void *))
+{
+    Vec *p = (Vec *)malloc(sizeof(Vec));
+    ASSERT(p);
+    p->len = 0;
+    p->capacity = 0;
+    p->elem_size = elem_size;
+    p->data = NULL;
+    p->cmp = cmp ? cmp : default_cmp;
+    p->grow = grow ? grow : default_growth_rate;
+    p->free_entry = free_entry ? free_entry : NULL;
+    vec_resize(p, capacity);
+    return p;
+}
+
+Vec *vec_new(size_t capacity, size_t elem_size, void_cmp_func cmp, vec_growth_rate_func grow, void (*free_entry)(const void *))
+{
+    Vec *p = malloc(sizeof(Vec));
+    ASSERT(p);
+    *p = (Vec){.elem_size = elem_size,
+               .capacity = 0,
+               .len = 0,
+               .data = NULL,
+               .cmp = cmp ? cmp : NULL,
+               .grow = grow ? grow : default_growth_rate,
+               .free_entry = free_entry ? free_entry : NULL};
+    vec_resize(p, capacity);
+    return p;
+}
+
+void vec_free(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    vec_clear(v);
+    free(v->data);
+    free(v);
+}
+
+void *vec_at(Vec *v, size_t index)
+{
+    return (void *)(&v->data[index * v->elem_size]);
+}
+
+void *vec_at_s(Vec *v, size_t index)
+{
+    VALIDATE_VECTOR(v);
+    if (index >= v->len)
+    {
+        return NULL;
+    }
+    return vec_at(v, index);
+}
+
+void vec_resize(Vec *v, size_t new_cap)
+{
+    VALIDATE_VECTOR(v);
+    if (new_cap == v->capacity)
+        return;
+    if (!new_cap)
+        new_cap++;
+    byte *new_data = (byte *)realloc(v->data, new_cap * v->elem_size * sizeof(byte));
+    ASSERT(new_data != NULL && "vec_resize: Failed to resize vec array.");
+
+    // Initialize the newly allocated memory
+    size_t old_cap_start = v->capacity * v->elem_size * sizeof(byte);
+    if (old_cap_start < new_cap)
+    {
+        size_t region_len = new_cap - v->capacity;
+        memset(new_data + old_cap_start, 0, region_len * v->elem_size * sizeof(byte));
+    }
+    v->capacity = new_cap;
+    v->data = new_data;
+}
+
+void vec_push_back(Vec *v, void *data)
+{
+    VALIDATE_VECTOR(v);
+    if (!data)
+        return;
+    if (v->len >= v->capacity)
+        vec_resize(v, v->grow(v));
+    memcpy(vec_at(v, v->len), data, v->elem_size * sizeof(byte));
+    v->len++;
+}
+
+void vec_sort(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    if (!v->cmp)
+    {
+        perror("vec_sort: Compare function is undefined.");
+        return;
+    }
+    qsort(v->data, v->len, v->elem_size, v->cmp);
+}
+
+void vec_insert(Vec *v, size_t index, void *data)
+{
+    VALIDATE_VECTOR(v);
+    if (index > v->len)
+        return;
+
+    if (v->len >= v->capacity)
+        vec_resize(v, v->grow(v));
+
+    memmove(vec_at(v, index + 1), vec_at(v, index), (v->len - index) * v->elem_size * sizeof(byte));
+    memcpy(vec_at(v, index), data, v->elem_size);
+
+    v->len++;
+}
+
+void vec_clear(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    if (v->free_entry)
+    {
+        V_FOR_EACH(v, void, var)
+        {
+            v->free_entry(var);
+        }
+    }
+    memset(v->data, 0, v->capacity * v->elem_size * sizeof(byte));
+    v->len = 0;
+}
+
+// void vec_clear(Vec *v)
+// {
+//     VALIDATE_VECTOR(v);
+//     memset(v->data, 0, v->capacity * v->elem_size * sizeof(byte));
+//     v->len = 0;
+// }
+
+void vec_clamp(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    byte *tmp;
+    if (!v->len)
+        tmp = (byte *)realloc(v->data, v->elem_size * sizeof(byte));
+    else
+        tmp = (byte *)realloc(v->data, v->len * v->elem_size * sizeof(byte));
+    ASSERT(tmp);
+    v->data = tmp;
+    v->capacity = v->len;
+}
+
+void *vec_pop_back(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    if (v->len < 1)
+        return NULL;
+    return vec_at(v, --v->len);
+}
+
+void *vec_find(Vec *v, void *_find)
+{
+    VALIDATE_VECTOR(v);
+    if (!v->cmp)
+    {
+        perror("vec_find: Compare function is undefined.");
+        return NULL;
+    }
+    for (size_t i = 0; i < v->len; i++)
+    {
+        if (v->cmp(vec_at(v, i), _find) == 0)
+        {
+            return vec_at(v, i);
+        }
+    }
+    return NULL;
+}
+
+void vec_remove(Vec *v, size_t index)
+{
+    VALIDATE_VECTOR(v);
+    if (index >= v->len)
+        return;
+    if (v->fe_idx != INVALID_FE_IDX)
+    {
+        v->fe_idx--;
+    }
+    if (index < v->len - 1)
+    {
+        memmove(vec_at(v, index), vec_at(v, index + 1), (v->len - index - 1) * v->elem_size * sizeof(byte));
+    }
+
+    v->len--;
+}
+
+void vec_remove_fast(Vec *v, size_t index)
+{
+    VALIDATE_VECTOR(v);
+    if (index >= v->len)
+        return;
+    if (v->fe_idx != INVALID_FE_IDX) // allows V_FOR_EACH to remove entries
+    {
+        v->fe_idx--;
+    }
+    memcpy(vec_at(v, index), vec_at(v, v->len - 1), v->elem_size * sizeof(byte));
+    v->len--;
+}
+
+/* Returns heap allocated deep copy */
+Vec *vec_copy(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    Vec *ret = (Vec *)malloc(sizeof(Vec));
+    ASSERT(ret);
+    ret->grow = v->grow;
+    ret->cmp = v->cmp;
+    ret->elem_size = v->elem_size;
+    ret->len = 0;
+    ret->data = NULL;
+    ret->capacity = 0;
+    vec_resize(ret, v->capacity);
+    if (v->capacity == 0)
+    {
+        return ret;
+    }
+    ASSERT(ret->data && ret->capacity == v->capacity);
+    memcpy(ret->data, v->data, v->capacity);
+    return ret;
+}
+
+void vec_reverse(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    byte *start = v->data;
+    byte *end = start + v->capacity - 1;
+
+    while (start < end)
+    {
+        byte temp = *start;
+        *start++ = *end;
+        *end-- = temp;
+    }
+}
+
+void vec_swap(Vec *v, size_t idx0, size_t idx1)
+{
+    VALIDATE_VECTOR(v);
+    if (idx0 >= v->len)
+        return;
+    if (idx1 >= v->len)
+        return;
+
+    size_t write_size = v->elem_size;
+    byte *idx0_entry = vec_at(v, idx0);
+    byte *idx1_entry = vec_at(v, idx1);
+    while (write_size)
+    {
+        size_t idx = write_size - 1;
+        byte tmp = idx0_entry[idx];
+        idx0_entry[idx] = idx1_entry[idx];
+        idx1_entry[idx] = tmp;
+        write_size--;
+    }
+}
+
+void *vec_pop_front(Vec *v)
+{
+    VALIDATE_VECTOR(v);
+    if (v->len < 1)
+        return NULL;
+    void *ret = vec_at(v, 0);
+    vec_remove(v, 0);
+    return ret;
+}
+
+size_t vec_size(Vec *v)
+{
+    return v->len;
+}
+
+int vec_append(Vec *dest, Vec *source)
+{
+    if (dest->elem_size != source->elem_size)
+        return 1;
+    for (size_t i = 0; i < source->capacity; i++)
+    {
+        void *data = vec_at_s(source, i);
+        if (!data)
+            return 2;
+        vec_push_back(dest, data);
+    }
+    return 0;
+}
---
+++ vector copy.h
@@ -0,0 +1,122 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+#include <stdint.h>
+
+/*
+    INFO:
+        The Vector is an array of bytes.
+        When adding a new member to the vector you must provide a valid memory address to copy the data from.
+        The vector will then contain a copy of the data at that index.
+        If the vector contains pointers to memory, you need to be careful to free memory before you remove the entry from the array.
+
+    TODO:
+        Add another header and c file, copy and paste this vector into there
+        and rewrite it to return errors.
+*       Don't just change these files because I link directly to to these files in some projects.
+*/
+/*
+    Define DISABLE_ASSERT to not use asserts.
+*/
+
+#define VECTOR_DEFAULT_CAP 10
+
+#define INVALID_FE_IDX ((size_t)-1)
+
+typedef uint8_t byte;
+
+typedef struct Vec Vec;
+
+/* 0 if eq, -1 if less, 1 if greater than */
+typedef int (*void_cmp_func)(const void *, const void *);
+
+/* returns the new capcaity based on the current vec state */
+typedef size_t (*vec_growth_rate_func)(Vec *);
+
+struct Vec
+{
+    byte *data;
+    size_t len;       // current number of entries
+    size_t capacity;  // max number of entries
+    size_t elem_size; // elem_size * capacity is the current memory allocation in bytes
+    void_cmp_func cmp;
+    vec_growth_rate_func grow;
+    void (*free_entry)(const void *);
+    size_t fe_idx; // use by VEC_FOR_EACH to ensure index after altering the vector
+};
+
+#define VEC(type) (vec_new(VECTOR_DEFAULT_CAP, sizeof(type), NULL, NULL, NULL))
+#define V_ADD(v, data) (vec_push_back(v, data))
+#define V_INS(v, idx, data) (vec_insert(v, idx, data))
+#define V_RM(v, idx) (vec_remove(v, idx))
+#define V_RMF(v, idx) (vec_remove_fast(v, idx))
+#define V_AT(v, index, t) ((index < (v)->capacity) ? (t *)vec_at(v, index) : NULL)
+#define V_CONT(v, data) (vec_find(v, data) ? 1 : 0)
+#define V_POP(v) (vec_pop_back(v))
+#define V_POP_FRONT(v) (vec_pop_front(v))
+#define V_COPY(v) (vec_copy(v))
+#define V_CLEAR(v) (vec_clear(v))
+#define V_REV(v) (vec_reverse(v))
+#define V_IS_ERR(err) ((err) != NONE)
+/* Accommodates adding and removing entries but not inserting before the current index.
+    You can still insert before the current index, but it will make you iterate over the current entry again
+    and you will not be able to iterate over the entry you inserted before the current index. */
+#define V_FOR_EACH(vec, type, var_name)                 \
+    (vec)->fe_idx = 0;                                  \
+    for (type *var_name = vec_at_s(vec, (vec)->fe_idx); \
+         var_name != NULL;                              \
+         var_name = vec_at_s(vec, ++((vec)->fe_idx)))
+
+typedef enum
+{
+    NONE,
+    IDX_OOB,
+    NULL_ARG,
+    NULL_VEC,
+    EMPTY_VEC,
+    FAILED_ALLOC,
+    MISSING_INTERNAL_FUNC,
+    UNKNOWN
+} Vec_Error;
+
+const char* vec_error_to_string(Vec_Error err);
+
+Vec_Error vec_new(Vec* ret, size_t capacity, size_t elem_size, void_cmp_func cmp, vec_growth_rate_func grow, void (*free_entry)(const void *));
+void vec_free(Vec *v);
+/* removes all entries, does not free memory */
+void vec_clear(Vec *v);
+/* data must be a valid memory address, it will be copied into the array */
+Vec_Error vec_push_back(Vec *v, void *data);
+/* ensures the current order of the vec */
+Vec_Error vec_remove(Vec *v, size_t index);
+/*  doesn't ensure the vec order */
+Vec_Error vec_remove_fast(Vec *v, size_t index);
+Vec_Error vec_resize(Vec *v, size_t new_size);
+Vec_Error vec_sort(Vec *v);
+/* data must be a valid memory address, it will be copied into the array */
+Vec_Error vec_insert(Vec *v, size_t index, void *data);
+/* resizes array to len */
+Vec_Error vec_clamp(Vec *v);
+Vec_Error vec_swap(Vec *v, size_t idx0, size_t idx1);
+void vec_reverse(Vec *v);
+/* Has return type */
+void *vec_at(Vec *v, size_t index);
+/* safer */
+void *vec_at_s(Vec *v, size_t index);
+// "returns" a ptr of type 't' at 'index' from 'v'
+/* NULL on fail, ptr to element on success */
+void *vec_pop_back(Vec *v);
+/* NULL on fail, ptr to element on success */
+void *vec_pop_front(Vec *v);
+/* NULL on fail, ptr to element on success */
+void *vec_find(Vec *v, void *_find);
+/* Returns heap allocated deep copy */
+Vec *vec_copy(Vec *v);
+/* returns the size of the vec */
+size_t vec_size(Vec *v);
+/*
+    Can return NONE, NULL_VEC, NULL_ARG, FAILED_ALLOC,
+*/
+Vec_Error vec_append(Vec *dest, Vec *source);
+
+#endif /* VECTOR_H */
